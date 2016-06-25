a2bbee3746fdf812973fd0a82372a8388b5036ba
Derek Damron <damrondm@gmail.com>
2016-06-25 16:26:44 -0700

    plot.budget - Add tests

---
+++ tests/testthat/test_plot_budget_1_args.R
@@ -0,0 +1,32 @@
+context("plot_budget")
+
+paycheck <- create_item( name = "Paycheck"
+                       , amount = 1050
+                       , day = 1
+                       , recurring = TRUE
+                       )
+rent <- create_item( name = "Rent"
+                   , amount = -800
+                   , day = 1
+                   , recurring = TRUE
+                   )
+internet <- create_item( name = "Internet"
+                       , amount = -100
+                       , day = 15
+                       , recurring = TRUE
+                       )
+my_bills <- create_schedule(paycheck, rent, internet)
+my_budget <- create_budget( my_bills
+                          , start=as.Date("2016-01-02")
+                          , end=as.Date("2016-01-02") + 30
+                          , initial=500
+                          )
+
+test_that("Check", {
+  expect_error(plot.budget(),
+              "Please provide a budget to plot")
+  expect_error(plot.budget(1:2),
+              "Only one budget should be provided")
+  expect_error(plot.budget(1),
+              "The object provided is not a budget")
+})

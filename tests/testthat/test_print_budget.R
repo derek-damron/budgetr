58101d1b350df714c1a6c2c3cabb3cdae2866e16
Derek Damron <damrondm@gmail.com>
2016-07-01 22:55:18 -0700

    print.budget - Fix tests

--- tests/testthat/test_print_budget.R
+++ tests/testthat/test_print_budget.R
@@ -26,13 +26,13 @@ test_that("Check", {
   expect_output(print(my_budget),
                "budget")
   expect_output(print(my_budget),
-               "day[ ]+date[ ]+name[ ]+amount[ ]+recurring[ ]+balance")
+               "date[ ]+name[ ]+balance")
   expect_output(print(my_budget),
-               "2[ ]+2016-01-02[ ]+Initial Amount[ ]+500[ ]+FALSE[ ]+500")
+               "2016-01-02[ ]+Initial Amount[ ]+500")
   expect_output(print(my_budget),
-               "15[ ]+2016-01-15[ ]+Internet[ ]+-100[ ]+TRUE[ ]+400")
+               "+2016-01-15[ ]+Internet[ ]+400")
   expect_output(print(my_budget),
-               "1[ ]+2016-02-01[ ]+Paycheck[ ]+1050[ ]+TRUE[ ]+1450")
+               "2016-02-01[ ]+Paycheck[ ]+1450")
   expect_output(print(my_budget),
-               "1[ ]+2016-02-01[ ]+Rent[ ]+-800[ ]+TRUE[ ]+650")
+               "2016-02-01[ ]+Rent[ ]+650")
 })
